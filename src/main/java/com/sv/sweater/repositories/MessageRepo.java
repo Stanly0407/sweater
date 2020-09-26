1730292ff761999666e2dd0530881df19c60d04d
Stanly0407 <Shelestova_S_V@mail.ru>
2020-09-27 00:38:31 +0300

    2 tutor.

---
+++ src/main/java/com/sv/sweater/repositories/MessageRepo.java
@@ -0,0 +1,13 @@
+package com.sv.sweater.repositories;
+
+import com.sv.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
+}
