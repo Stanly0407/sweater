0c686356c641b9259f326eec5eab42db960cef2d
Stanly0407 <Shelestova_S_V@mail.ru>
2020-09-27 23:36:55 +0300

    freemarker

--- src/main/java/com/sv/sweater/controllers/RegistrationController.java
+++ src/main/java/com/sv/sweater/controllers/RegistrationController.java
@@ -5,7 +5,6 @@ import com.sv.sweater.domain.Role;
 import com.sv.sweater.domain.User;
 import com.sv.sweater.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerTemplateAvailabilityProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/sv/sweater/repositories/UserRepo.java
+++ src/main/java/com/sv/sweater/repositories/UserRepo.java
@@ -2,7 +2,6 @@ package com.sv.sweater.repositories;
 
 import com.sv.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
