4ed1d273278492e6eaf135b827c2762bfa688c86
Stanly0407 <Shelestova_S_V@mail.ru>
2020-09-27 14:00:14 +0300

    spring security

--- src/main/java/com/sv/sweater/GreetingController.java
+++ src/main/java/com/sv/sweater/GreetingController.java
@@ -18,21 +18,20 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
@@ -45,7 +44,7 @@ public class GreetingController {
     public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
 
-        if (filter !=null && !filter.isEmpty()){
+        if (filter != null && !filter.isEmpty()) {
             messages = messageRepo.findByTag(filter);
         } else {
             messages = messageRepo.findAll();
