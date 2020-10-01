c6570df6322261170be4cae55395409a0d2b4621
Stanly0407 <Shelestova_S_V@mail.ru>
2020-10-02 01:07:40 +0300

    been validation

--- src/main/java/com/sv/sweater/controllers/RegistrationController.java
+++ src/main/java/com/sv/sweater/controllers/RegistrationController.java
@@ -5,10 +5,12 @@ import com.sv.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -18,14 +20,26 @@ public class RegistrationController {
     private UserService userService;
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addNewUser(User user, Map<String, Object> model){
-              if (!userService.addUser(user)) {
-            model.put("message", "User exists!");
+    public String addNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+        // проверяем верно ли повторно юзер ввел пароль
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+            model.addAttribute("passwordError", "passwords are different!");
+        }
+ // если в bindingResult будут ошибки - обрабатываем их
+        if(bindingResult.hasErrors()){
+            //переводим в текст. коллект.
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+            return "registration";
+        }
+
+        if (!userService.addUser(user)) {
+            model.addAttribute("usernameError", "User exists!");
             return "registration";
         }
         return "redirect:/login";
@@ -33,12 +47,12 @@ public class RegistrationController {
 
     // обработка подтверждения аккаунта пользователя
     @GetMapping("/activate/{code}")
-    public String activate(Model model, @PathVariable String code){
+    public String activate(Model model, @PathVariable String code) {
         boolean isActivated = userService.activateUser(code);
         // сообщаем юзеру о том как прошла активация
         if (isActivated) {
             model.addAttribute("message", "User successfully activated.");
-                    } else {
+        } else {
             model.addAttribute("message", "Activation code is not found!");
         }
 
