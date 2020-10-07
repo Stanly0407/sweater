a1601761aa86a1a38ee99dfa32b75c8ae19aaed9
Stanly0407 <Shelestova_S_V@mail.ru>
2020-10-07 23:47:51 +0300

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,11 @@ echo 'Copy files...'
 
 scp -i ~/.ssh/id_rsa_drucoder \
     target/sweater-1.0-SNAPSHOT.jar \
-    dru@192.168.0.107:/home/dru/
+    sv@192.168.100.4:/home/sv/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa_drucoder dru@192.168.0.107 << EOF
+ssh -i ~/.ssh/id_rsa_drucoder sv@192.168.100.4 << EOF
 pgrep java | xargs kill -9
 nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
 EOF
