b0afd4f869b85fda83eae2e107b2425f5c70ccd0
Stanly0407 <Shelestova_S_V@mail.ru>
2020-10-10 16:23:08 +0300

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/id_rsa_drucoder \
+scp -i ~/.ssh/id_rsa \
     target/sweater-1.0-SNAPSHOT.jar \
     sv@192.168.100.4:/home/sv/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa_drucoder sv@192.168.100.4 << EOF
+ssh -i ~/.ssh/id_rsa sv@192.168.100.4 << EOF
 pgrep java | xargs kill -9
 nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
 EOF
