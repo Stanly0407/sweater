bdab008c1c1f632671896e8428d99abd54253173
Stanly0407 <Shelestova_S_V@mail.ru>
2020-10-11 23:55:28 +0300

    Testing the Web Layer

--- src/test/java/com/sv/sweater/LoginTest.java
+++ src/test/java/com/sv/sweater/LoginTest.java
@@ -1,5 +1,6 @@
 package com.sv.sweater;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,5 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginTest {
+
+    @Test
+    public void test() throws Exception {
+
+    }
 
 }
