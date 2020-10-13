25e0545920af3fa3d8c8c1c26d5f21c653d0b594
Stanly0407 <Shelestova_S_V@mail.ru>
2020-10-13 22:49:25 +0300

    integration testing -  final

--- src/test/java/com/sv/sweater/MainControllerTest.java
+++ src/test/java/com/sv/sweater/MainControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -71,11 +72,12 @@ public class MainControllerTest {
         MockHttpServletRequestBuilder multipart = multipart("/main")
                 .file("file", "123".getBytes())
                 .param("text", "fifth")
-                .param("tag", "new one");
+                .param("tag", "new one")
+                .with(csrf());
         this.mockMvc.perform(multipart)
                 .andDo(print())
                 .andExpect(authenticated())
-                .andExpect(xpath("//div[@id='message-list']/div").nodeCount(2)) // это будет месседж с id = 5 и элементов будет выведено 5
+                .andExpect(xpath("//div[@id='message-list']/div").nodeCount(5)) // это будет месседж с id = 5 и элементов будет выведено 5
                 .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]").exists()) // т.к. нумерацию установили с 10
                 .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]/div/span").string("fifth"))
                 .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]/div/i").string("#new one"));
--- src/test/resources/messages-list-before.sql
+++ src/test/resources/messages-list-before.sql
@@ -1,4 +1,5 @@
 delete from message;
+delete from hibernate_sequence;
 
 insert into message(id, text, tag, user_id) values
 (1, 'first', 'my-tag', 1),
@@ -6,6 +7,8 @@ insert into message(id, text, tag, user_id) values
 (3, 'third', 'my-tag', 1),
 (4, 'fourth', 'another', 1);
 
+
+--alter table message auto_increment 10;
 insert into hibernate_sequence values (10);
 
 -- alter sequence hibernate_sequence restart with 10;
